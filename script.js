9d44388e9320200cc6e5b4281b9734a7cb3062e2
Seamor Estrabon <seamorestrabon@gmail.com>
2024-12-02 00:22:27 +0800

    fix: syntax error for cors

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const getLocation = async () => {
     const response = await fetch(
       "https://api.ipgeolocation.io/ipgeo?apiKey=159234c99fb545ada50544206108bb6b",
       {
-        mode: cors,
+        mode: "cors",
       }
     );
     const data = await response.json();
@@ -13,12 +13,14 @@ const getLocation = async () => {
   }
 };
 
+getLocation();
+
 const getWeatherData = async () => {
   try {
     const response = await fetch(
       "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Bacolod%2520City?include=fcst%2Cobs%2Chistfcst%2Cstats%2Cdays%2Chours%2Ccurrent%2Calerts&key=WKF2HYP55LTVJTKM3F2Q6NBF9&options=beta&contentType=json",
       {
-        mode: cors,
+        mode: "cors",
       }
     );
     const data = await response.json();
